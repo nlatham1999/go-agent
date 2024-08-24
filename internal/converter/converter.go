50d24b4d4982432759306bfaa2146ebcc8b08739
nlatham1999 <nicholaslatham1999@gmail.com>
2024-08-23 19:08:18 -0700

    adding tokens

---
+++ internal/converter/converter.go
@@ -0,0 +1,12 @@
+package converter
+
+func Convert(filename string) error {
+
+	//open the file
+	// file, err := os.Open(filename)
+	// if err != nil {
+	// 	return fmt.Errorf("could not open file: %v", err)
+	// }
+
+	return nil
+}
